aeb8fd18746e20b64b45dd4f7ac6e07340e6d28c
Mehmet Ali Ayvaz <mehmetaliayvaz61@gmail.com>
2023-01-29 19:39:22 +0300

    install tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "992px",
+      xl: "1080px",
+      "2xl": "1344px",
+    },
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+        },
+      },
+      colors: {},
+      borderRadius: {},
+      backgroundImage: {},
+      fontSize: {},
+      fontFamily: {
+        poppins: ['"Poppins"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
